7eca1c52eb7d962e75f82ca4705ffad66cca315d
switters <codenomadic@gmail.com>
2014-10-10 15:58:36 -0700

    Adds instantiation and internals testing.

--- test/index.js
+++ test/index.js
@@ -2,26 +2,74 @@
 /**
  * Dependencies
  */
-var Retsly = require('retsly-js-sdk');
-var Auth = require('retsly-js-auth');
+var _ = require('underscore');
+var $ = require('jquery');
+
+// Construct a mock retsly-js-sdk isntance
+var f = function(){};
+f.getUserToken = function(){ return 'xxxxx'; };
+f.get = function () { return 'mocked response'; };
+f.getDomain = function() { return 'https://stg.rets.ly:441'; };
+f.getClient = function() { return 'yyyyyyy'; };
+
+var Auth = require('retsly-js-auth')(f);
+var Backbone = require('backbone');
+Backbone.$ = window.$;
+
 var assert = require('assert');
-
-// requires a retsly instance
-// must have a redirectURI
-// must have an authorized cb
-//
-// test the returned component
-
-// && muchmuch
-// moar!
 
 
 /**
  * Tests
  */
-suite('Auth')
-test('cannot be required without instantiated retsly', function() {
+var options;
+
+beforeEach(function(){
+  options = {
+    el: '.tests',
+    redirect_uri: 'www.something.com/callback',
+    selector: '.duration',
+    authorized: function () { return 'authed.'; }
+  };
+});
+
+
+suite('Auth component');
+test('cannot be required without injecting retsly', function() {
   assert.throws(function() {
-    require('retsly-js-auth')()
+    require('retsly-js-auth')();
   });
+});
+
+
+suite('Auth component instantiation');
+test('will fail without passing a redirect_uri', function () {
+  var fail = delete options.redirect_uri;
+  assert.throws(function() {
+    new Auth(fail);
+  });
+});
+
+test("will fail without passing an 'authorized' function", function () {
+  var fail = delete options.authorized;
+  assert.throws(function() {
+    new Auth(fail);
+  });
+});
+
+
+suite('Auth component internals');
+test('include an initialize function', function() {
+  var auth = new Auth(options);
+  assert('function' == typeof auth.initialize);
+});
+
+test('include an dialog function', function() {
+  var auth = new Auth(options);
+  assert('function' == typeof auth.dialog);
+});
+
+test('include an render function', function() {
+  var auth = new Auth(options);
+  assert('function' == typeof auth.render);
 });
